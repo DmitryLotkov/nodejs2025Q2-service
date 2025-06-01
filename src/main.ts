b81967e3b0fe09fe061e8e3f682b74a3b2e52aee
Lotkovsky <dmitry.lotkov9@gmail.com>
2025-06-01 18:49:32 +0300

    fix: env port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filter/http-exeption.filter';
 
+const PORT = process.env.PORT || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 void bootstrap();
